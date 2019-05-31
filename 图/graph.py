fcc0cfcf19bb555299ab2d8f38ef3fd7373d25ac
Frank Lee <seaton_xi@outlook.com>
2019-05-31 21:26:36 +0800

    correct method get_edge

--- 图/graph.py
+++ 图/graph.py
@@ -4,13 +4,12 @@ class GraphError(Exception):
     pass
 
 class Graph:
-    def __init__(self, dic: Dict[int,list]={}, unconn=0):
+    def __init__(self, dic: Dict[int,list]={}):
         self.__vnum = 0
-        self.__table = {} # table以链接表的形式存储图，形如{0:[vertex, weight]}
-        self.unconn = unconn
+        self.__table = {} # table以链接表的形式存储图，形如{0:[(vertex, weight)]}
         for v, e in dic.items():
-            e.sort()
-            self.__table[v] = e
+            e.sort() # 默认出边列表元素的第一项是节点，第二项是权重
+            self.__table[v] = list(e)
             self.__vnum += 1
             
     @property
@@ -20,7 +19,7 @@ class Graph:
     def is_empty(self):
         return self.__vnum == 0
         
-    def get_vertex(self):
+    def get_vertex(self):   # 考虑到节点不一定是连续的数
         return [v for v in self.__table]
     
     def add_vertex(self, v:int):
@@ -46,10 +45,24 @@ class Graph:
         e = self.__table.get(v)
         if e is None:
             raise GraphError("Graph doesn\'t have vertex %s"%vi)
-        return self.__table.get(v)
+        return self.__table[v]
         
-    def get_edges(self, vi:int, vj:int):
+    def get_edge(self, vi:int, vj:int):
         e = self.__table.get(vi)
-        if e is None or vj<0 or vj>len(e):
+        lens = len(e)
+        if e is None:
             raise GraphError("Graph doesn\'t have vertex %s"%vi)
-        return e[vj]
+        elif vj < 0 or vj > self.__vnum:
+            raise GraphError("Graph doesn\'t have vertex %s"%vj)
+        if vi == vj:
+            return 0    # 节点到自己的距离为零
+        l, r = 0, lens-1
+        while l < r:    # 二分查找
+            mid = (l + r)>>1
+            if e[mid][0] >= vj:
+                r = mid
+            else:
+                l = mid + 1
+        if e[l][0] == vj:
+            return e[l][1]
+        return float('inf') # 不存在的边用无限大表示
