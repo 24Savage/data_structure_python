0e81ed8bcc69989f247ad8e7fc92a05d777eb407
Frank Lee <seaton_xi@outlook.com>
2019-06-05 23:57:41 +0800

    fix _shiftup, _shiftdown method

--- 树/prio_queue.py
+++ 树/prio_queue.py
@@ -2,23 +2,23 @@ from math import log
 
 class PrioQueue:
     def __init__(self, seq=list()):
-        self.__elem = list(seq)
-        self.__size = len(self.__elem)
-        self.__makeheap()
+        self._elem = list(seq)
+        self._size = len(self._elem)
+        self._makeheap()
         
     def is_empty(self):
-        return self.__size == 0
+        return self._size == 0
         
     def enqueue(self,val):
-        self.__elem.append(val)
-        self.__size += 1
-        self.__shiftup()
+        self._elem.append(val)
+        self._shiftup(self._size)
+        self._size += 1
     
-    def __shiftup(self):
-        elem= self.__elem
-        i = self.__size-1
+    def _shiftup(self,start):
+        elem= self._elem
+        i = start
         j = (i-1)//2
-        e = elem[-1]
+        e = elem[i]
         while j > -1:
             if elem[j] <= e:
                 break
@@ -27,22 +27,22 @@ class PrioQueue:
         elem[i] = e
     
     def dequeue(self):
-        if not self.__size:
+        if not self._size:
             raise IndexError('pop from empty prioQueue')
-        elem = self.__elem
+        elem = self._elem
         e0 = elem[0]
         e = elem.pop()
-        self.__size -= 1
-        if self.__size: # 只有一个元素时，无需进行向下交换
+        self._size -= 1
+        if self._size: # 只有一个元素时，无需进行向下交换
             elem[0] = e
-            self.__shiftdown(0)
+            self._shiftdown(0)
         return e0
         
-    def __shiftdown(self, start):
-        elem = self.__elem
+    def _shiftdown(self, start):
+        elem = self._elem
         i, j = start, (start*2+1)
-        end = self.__size
-        e = elem[0]
+        end = self._size
+        e = elem[i]
         while j < end:
             if j+1 < end and elem[j+1] < elem[j]:
                 j += 1
@@ -52,17 +52,17 @@ class PrioQueue:
             i, j = j, 2*j+1
         elem[i] = e
         
-    def __makeheap(self):
-        elem = self.__elem
-        length = self.__size
+    def _makeheap(self):
+        elem = self._elem
+        length = self._size
         for i in range(length//2-1, -1, -1):
-            self.__shiftdown(i)
+            self._shiftdown(i)
             
     def show(self):
-        h = log(self.__size, 2)//1
+        h = log(self._size, 2)//1
         h = int(h)
-        elem = self.__elem
+        elem = self._elem
         print(elem[:1])
         for i in range(1, h):
             print(elem[2**i-1 : 2**(i+1)-1])
-        print(elem[2**h-1 :], '\n', '-'*self.__size)
+        print(elem[2**h-1 :], '\n', '-'*self._size)
